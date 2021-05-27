a2383e010295ab046446fe8697edbcd61b086d27
Liz Byler <>
2021-05-27 02:33:12 +0000

    defined variables added question added answer

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space?";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -21,7 +21,7 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+question = input.question ("Who was the first American woman in space?");
 
 }
 
