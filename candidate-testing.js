929d5686dbe4bc48ceeed7552ac214bd23c53cff
Liz Byler <>
2021-06-05 22:54:23 +0000

    set arrays, pushed answers to arrays, removed case sensitive comparison, calculated grade

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,12 +5,12 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space?";
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ['1) Who was the first American woman in space? ', '2) True or false: 5 kilometer == 5000 meters? ', '3) (5 + 3)/2 * 10 = ?' , '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?' , '5) What is the minimum crew size for the ISS?'];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -21,8 +21,13 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question ("Who was the first American woman in space?");
+//candidateAnswer = input.question ("Who was the first American woman in space?");//
+for (let i = 0; i < questions.length; i++) {
 
+ candidateAnswer = input.question (questions[i]);
+ candidateAnswers.push(candidateAnswer);
+ //console.log(candidateAnswers); //
+}
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -30,17 +35,47 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  let grade;
-  //console.log (candidateAnswer);//
-  //console.log (correctAnswer);//
-if (candidateAnswer === correctAnswer){
-  grade = "Pass";
+  let grade = 0;
+  let overallGrade;
+console.log('');
+console.log('');
+console.log('');
+  console.log(`Candidate Name: ${candidateName}`);
+  for (let i = 0; i < candidateAnswers.length; i++) {
+
+  if (candidateAnswers[i].toUpperCase() === correctAnswers[i].toUpperCase()){
+  console.log(questions[i]);
+    console.log(`Your Answer: ${candidateAnswers[i]}`);
+    console.log(`Correct Answer: ${correctAnswers[i]}`);
+    console.log('');
+  grade++ ;
 
 }else {
-  grade = "Fail";
+ console.log(questions[i]);
+    console.log(`Your Answer: ${candidateAnswers[i]}`);
+    console.log(`Correct Answer: ${correctAnswers[i]}`);
+    console.log('');
+}  
+  }
+
+  //console.log (candidateAnswer);//
+  //console.log (correctAnswer);//
+//if (candidateAnswer === correctAnswer){
+  //grade = "Pass";
+
+//}else {
+  //grade = "Fail";
+//}
+overallGrade = (grade/correctAnswers.length)*100;
+//console.log(overallGrade);
+console.log (`>>>Overall Grade: ${overallGrade}% (${grade} of ${correctAnswers.length} responses correct) <<<`);
+
+if(overallGrade >=80){
+console.log('>>>Status: PASSED <<<')
+}else{
+  console.log('>>>Status: FAILED <<<')
 }
 
-  return grade;
 }
 
 function runProgram() {
